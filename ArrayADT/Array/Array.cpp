5e0693963471c388440358f2f930387a707a4950
Hector <hectoremil21@hotmail.com>
2021-04-26 20:43:06 -0400

    Get() Set() Max() Min() Sum() Avg() functions.

--- ArrayADT/Array/Array.cpp
+++ ArrayADT/Array/Array.cpp
@@ -106,21 +106,65 @@ int RBinarySearch(struct Array* arr, int l, int h, int key) {
     return -1;
 }
 
+int Get(struct Array arr, int index) {
+    if (index >= 0 && index < arr.length)
+        return arr.A[index];
+    return -1;
+}
+
+void Set(struct Array* arr, int index, int x) {
+    if (index >= 0 && index < arr->length)
+        arr->A[index] = x;
+}
+
+int Max(struct Array arr) {
+    int max = arr.A[0];
+    int i;
+    for (i = 1; i < arr.length; i++) {
+        if (arr.A[i] > max)
+            max = arr.A[i];
+    }
+    return max;
+}
+
+int Min(struct Array arr) {
+    int min = arr.A[0];
+    int i;
+    for (i = 1; i < arr.length; i++) {
+        if (arr.A[i] < min)
+            min = arr.A[i];
+    }
+    return min;
+}
+
+int Sum(struct Array arr) {
+    int s = 0;
+    int i;
+    for (i = 0; i < arr.length; i++)
+        s += arr.A[i];
+    return s;
+}
+
+float Avg(struct Array arr) {
+    return (float) Sum(arr) / arr.length;
+}
+
 int main()
 {
     struct Array arr = { {2,3,4,5,6,10,12,14,29}, 20, 9 };
 
     // Do binary search before screwing up the sort.
+    // Linear.
     int siFound = LinearBinarySearch(&arr, 29);
     std::cout << "Found this item: " << siFound << std::endl;
 
     int notFound = LinearBinarySearch(&arr, 21);
     std::cout << "Couldn't find an item: " << notFound << std::endl;
-
-    int rSiFound = RBinarySearch(&arr, 0, arr.length, 29);
+    // Recursively
+    int rSiFound = RBinarySearch(&arr, 0, arr.length-1, 29);
     std::cout << "Found this item recursively: " << rSiFound << std::endl;
 
-    int rNotFound = RBinarySearch(&arr, 0, arr.length, 1);
+    int rNotFound = RBinarySearch(&arr, 0, arr.length-1, 1);
     std::cout << "Couldn't find an item recursively: " << notFound << std::endl;
     std::cout << std::endl;
 
