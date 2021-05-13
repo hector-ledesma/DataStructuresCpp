48948da29ac040704b0321e0f9b7ab3dbf459fcd
Hector <hectoremil21@hotmail.com>
2021-05-13 14:31:12 -0400

    Stack display method

--- LinkedList/LinkedList/Stack1.cpp
+++ LinkedList/LinkedList/Stack1.cpp
@@ -6,3 +6,21 @@ Stack1::Stack1(int sz) {
 	size = sz;
 	s = new int[sz];
 }
+
+void Stack1::Display() {
+	for (int i = top; i >= 0; i--)
+		std::cout << s[i] << " ";
+	std::cout << std::endl;
+}
+
+int Stack1::stackTop() {
+	// We want to know
+}
+
+bool Stack1::isEmpty() {
+
+}
+
+int Stack1::isFull() {
+
+}
