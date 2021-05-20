237f6d1762c9cf8f342d24e3de4601d10e21d032
Hector <hectoremil21@hotmail.com>
2021-05-20 10:43:29 -0400

    Set up xcode project.

---
+++ DataStructuresCpp/DataStructuresCpp/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  DataStructuresCpp
+//
+//  Created by Karen Rodriguez on 5/20/21.
+//
+
+#include <iostream>
+#import "LinkedList.h"
+#import "BinaryTree.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
