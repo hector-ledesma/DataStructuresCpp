ad5163ed8f3e6cde449ce5df057f3d568e0aaa54
Hector <hectoremil21@hotmail.com>
2021-05-27 13:53:41 -0400

    Linear Probing collision handling.

--- LinkedList/LinkedList/HashTable.cpp
+++ LinkedList/LinkedList/HashTable.cpp
@@ -8,6 +8,17 @@ HashTable::HashTable() {
 	table = new HNode * [10]{ nullptr };
 }
 
+HashTable::~HashTable() {
+	for (int i = 0; i < 10; i++) {
+		HNode* p = table[i];
+		while (table[i]) {
+			table[i] = table[i]->next;
+			delete p;
+			p = table[i];
+		}
+	}
+}
+
 int HashTable::hashmod(int key) {
 	return key % 10;
 }
@@ -79,13 +90,38 @@ void HashTable::runChaining() {
 	std::cout << "\n" << SearchChaining(68) << std::endl;
 }
 
-HashTable::~HashTable() {
-	for (int i = 0; i < 10; i++) {
-		HNode* p = table[i];
-		while (table[i]) {
-			table[i] = table[i]->next;
-			delete p;
-			p = table[i];
-		}
+/*
+	With linear probing, lambda/loading factor has to always be less than or equals to 5.
+	Meaning, the table array has to always be half empty at the very least.
+
+	We know if a search fails once we find empty space.
+
+	Deleting is always discouraged when using Linear Probing. This is because when deleting,
+	you'll have to rehash all your keys.
+	
+	So you're better off using a flag with your nodes to indicate whether they've been "Deleted" or not.
+*/
+int HashTable::hashprob(int key) {
+	return key % size;
+}
+void HashTable::InsertProbe(int H[], int key) {
+	int idx = hashprob(key);
+	// With Linear probe, we're not using linked lists, so we have to find a different spot to insert at.
+	if (H[idx] != 0) idx = LinearProbe(H, key);
+	H[idx] = key;
+}
+int HashTable::SearchProbe(int H[], int key) {
+	int idx = hashprob(key);
+	int i = 0;
+	while (H[(idx + 1) % size] != key) {
+		i++;
+		if (H[(idx + i) % size] == 0) return -1; // failure condition is if we go around the whole array.
 	}
+	return (idx + i) % size;
+}
+int HashTable::LinearProbe(int H[], int key) {
+	int idx = hashprob(key);
+	int i = 0;
+	while (H[(idx + i) % size] != 0) i++; // We basically just keep rehashing til we find an empty spot by adding 1 to i
+	return (idx + i) % size; // return index for first empty spot.
 }
--- LinkedList/LinkedList/HashTable.h
+++ LinkedList/LinkedList/HashTable.h
@@ -1,4 +1,8 @@
 #pragma once
+/*
+	Analysis of hashing is based on loading factor.
+		Loading factor = n/size
+*/
 class HNode {
 public:
 	int data;
@@ -8,13 +12,24 @@ public:
 };
 class HashTable
 {
-public:
+private:
 	HNode** table;
-	HashTable();
-	void runChaining();
+	int size = 10;
+	// Chaining Collision handling methods
 	int hashmod(int key);
 	void InsertChaining(int key);
 	int SearchChaining(int key);
+
+	// Linear probing collision
+	int hashprob(int key);
+	int LinearProbe(int H[], int key);
+	void InsertProbe(int H[], int key);
+	int SearchProbe(int H[], int key);
+
+
+public:
+	void runChaining();
+	HashTable();
 	~HashTable();
 };
 
