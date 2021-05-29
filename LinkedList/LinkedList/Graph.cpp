2b7f3d87fcfd7078ec42de3f45a270d74f785107
Hector <hectoremil21@hotmail.com>
2021-05-28 20:54:22 -0400

    Prim's Minimum Cost Spanning Tree algorithm.

--- LinkedList/LinkedList/Graph.cpp
+++ LinkedList/LinkedList/Graph.cpp
@@ -44,11 +44,106 @@ void Graph::DFS(int start) {
 				DFS(j); // we dive to the end of that extreme.
 	}
 }
+
+void Graph::PrimMST() {
+	int u, v; // These will simply serve as our indices for our edges. u and v are the vertices.
+	int min{ I };
+	int track[8]; // This is where we keep trck of visited vertices, as well as nearest edge.
+	int T[2][8 - 2]{ 0 }; // Technically I can just write 6, but I wanna keep clear that 8 simply represents size.
+	// The reason for -2 is that spanning trees have 1 less edge than there are vertices.
+	// But we're holding one extra space in the array so that we may index by value
+	// so -1 to subtract the extra index and -1 again for the spanning tree condition.
+	// 7 vertices, 6 edges.
+
+	// First step: Find cheapest edge by visiting all points int he matric on the top half.
+	for (int i{ 1 }; i < 8; i++) {
+		track[i] = I;
+		for (int j{ i }; j < 8; j++) {
+			if (wg[i][j] < min) { // Whenever we find a lower cost edge.
+				min = wg[i][j]; // Save how much it cost.
+				// Save the edge by storing both vertices.
+				u = i; 
+				v = j;
+			}
+		}
+	}
+	// We're saving our edges vertically
+	// So the second index T[][x] is an edge
+	// and the first index simply lets is mark origin and destination T[x][]
+	T[0][0] = u;
+	T[1][0] = v;
+	// Now we need to mark both these vertices as visited by marking them as 0
+	track[u] = track[v] = 0;
+
+	// Now we need to find the initial cost comparisons using our first 2 vertices
+	for (int i{ 1 }; i < 8; i++) {
+		if (track[i] != 0) { // If this index is 0 we skip it
+			// So we're using our index as our vertex as well.
+			// we compare the cost of the edge between our index and vertex u
+			// as well as the cost from i to v
+			if (wg[i][u] < wg[i][v]) track[i] = u; // if u is lower, we store u itself to indicate that the combination of
+			// wg[i]wg[track[i]] where track[i] is the vertex stored at that value, is our lower cost edge
+			else track[i] = v; // if v is larger or equals, we save v since it doesn't matter.
+		}
+	}
+
+	// Now comes the repeating process
+	// We start at 1 because we handled index 0 at the initial setup
+	for (int i{ 1 }; i < 8 - 2; i++) { // Spanning trees have one less edge than there are vertices. So we want to go from 1 to 5 because we have 7 vertices.
+		int k;
+		min = I;
+		// Now we need to find the min cost edge from all the edges we last found.
+		// since we've stored them in track, we can just use the index as one of our vertices, and stored value as the other.
+		for (int j{ 1 }; j < 8; j++) {
+			if (track[j] != 0 && wg[j][track[j]] < min) { // If our stored value is 0, then we already this index stored
+				// Once we find the smallest value edge
+				k = j; // we store the index at which we found it for later use.
+				min = wg[j][track[j]]; // and we store the cost of the edge
+			}
+		}
+
+		// Each index i represents the edge for our spanning tree.
+		T[0][i] = k; // At this index, the "u" vertex is the index at which we had just found the smallest cost edge.
+		T[1][i] = track[k]; // "v" vertex is the value stored at that index making our pair.
+		track[k] = 0; // mark this index as visited.
+
+		// Now we need to find which are our smallest cost edges.
+		//	Either the edges we had already found, or the ones that our newest vertex connects to
+		for (int j{ 1 }; j < 8; j++) {
+			if (track[j] != 0 ) { // If it's not an already added/visited vertex
+				// This could be stored in the if statement, but I wanted to separate it for legibility.
+				int ne = wg[j][k]; // This is the new edge. j as our index as well as vertex, and k as our newest added vertex
+				int oe = wg[j][track[j]]; // This is the old edge, which we simply have stored at j itself and value at j
+				// If our new edge is of less cost than our previously found edge, we're gonna want this new edge
+				if (ne < oe) track[j] = k; // So we store the new vertex as value. So now our pair will be index j plus stored k value.
+			}
+
+		}
+
+		printST(T);
+	}
+}
+void Graph::printST(int st[][8 - 2]) {
+	std::cout << "\nMinimum Spanning Tree Edges (w/ cost)" << std::endl;
+	int sum{ 0 };
+	for (int i{ 0 }; i < 8 - 2; i++) {
+		int u = st[0][i];
+		int v = st[1][i];
+		int cost = wg[u][v];
+		std::cout << "[" << u << "]--[" << v << "] cost: " << cost << std::endl;
+		sum += cost;
+	}
+	std::cout << std::endl;
+	std::cout << "Total cost of Minimum Cost Spanning Tree: " << sum << std::endl;
+}
+
 void Graph::runGraphs() {
 	std::cout << "\nBFS starting at 4" << std::endl;
 	BFS(4);
 
 	std::cout << "\nDFS starting at 4" << std::endl;
 	DFS(4);
+
+	PrimMST();
 }
 
--- LinkedList/LinkedList/Graph.h
+++ LinkedList/LinkedList/Graph.h
@@ -1,7 +1,8 @@
 #pragma once
-class Graph
-{
+#define I 32767
+class Graph {
 private:
+    // Undirected Graph
 	int g[7][7] = { {0,0,0,0,0,0,0},
                     {0,0,1,1,0,0,0},
                     {0,1,0,0,1,0,0},
@@ -12,6 +13,19 @@ private:
     const int sz = 7;
     void BFS(int start);
     void DFS(int start);
+    // Weighted Graph
+    int wg[8][8] {
+            {I, I, I, I, I, I, I, I},
+            {I, I, 25, I, I, I, 5, I},
+            {I, 25, I, 12, I, I, I, 10},
+            {I, I, 12, I, 8, I, I, I},
+            {I, I, I, 8, I, 16, I, 14},
+            {I, I, I, I, 16, I, 20, 18},
+            {I, 5, I, I, I, 20, I, I},
+            {I, I, 10, I, 14, 18, I, I},
+    };
+    void PrimMST();
+    void printST(int st[][8-2]);
 public:
     void runGraphs();
 };
